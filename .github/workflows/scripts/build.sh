c71d2cf814171ccec74b03c121b890be1ff088ba
AlpinDale <alpindale@gmail.com>
2025-01-27 12:19:25 +0000

    CI: use self-hosted runner for the build job

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -12,7 +12,7 @@ $python_executable -m pip install wheel packaging
 $python_executable -m pip install -r requirements-cuda.txt
 
 # Limit the number of parallel jobs to avoid OOM
-export MAX_JOBS=2
+export MAX_JOBS=28
 
 export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 
