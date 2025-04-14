7d0cfe988465e961db2ebcbc192ffe27d54b6040
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2025-04-15 01:43:27 +0430

    tpu: call `torch._sync(param)` during weight loading to reduce mem usage (#1297)

--- aphrodite/modeling/utils.py
+++ aphrodite/modeling/utils.py
@@ -5,6 +5,8 @@ from typing import Any, Dict, Optional
 import numpy as np
 import torch
 
+from aphrodite.platforms import current_platform
+
 
 def set_random_seed(seed: int) -> None:
     random.seed(seed)
@@ -32,4 +34,25 @@ def set_weight_attrs(
     for key, value in weight_attrs.items():
         assert not hasattr(
             weight, key), (f"Overwriting existing tensor attribute: {key}")
+
+        # NOTE: During weight loading, we often do something like:
+        # narrowed_tensor = param.data.narrow(0, offset, len)
+        # narrowed_tensor.copy_(real_weight)
+        # expecting narrowed_tensor and param.data to share the same storage.
+        # However, on TPUs, narrowed_tensor will lazily propagate to the base
+        # tensor, which is param.data, leading to the redundant memory usage.
+        # This sometimes causes OOM errors during model loading. To avoid this,
+        # we sync the param tensor after its weight loader is called.
+        # TODO: Remove this hack once we have a better solution.
+        if current_platform.is_tpu() and key == "weight_loader":
+            value = _make_synced_weight_loader(value)
         setattr(weight, key, value)
+
+
+def _make_synced_weight_loader(original_weight_loader):
+
+    def _synced_weight_loader(param, *args, **kwargs):
+        original_weight_loader(param, *args, **kwargs)
+        torch._sync(param)
+
+    return _synced_weight_loader
