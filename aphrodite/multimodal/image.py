e051c69697651c5cebef8885de52a047d485bf16
AlpinDale <alpindale@gmail.com>
2025-05-06 07:04:01 +0000

    multimodal refactor

--- aphrodite/multimodal/image.py
+++ aphrodite/multimodal/image.py
@@ -1,80 +1,75 @@
-from functools import lru_cache
-from typing import Any, Dict, Optional
+import base64
+from io import BytesIO
+from pathlib import Path
 
 import torch
-from loguru import logger
 from PIL import Image
-from transformers.image_processing_base import BatchFeature
 
-from aphrodite.common.config import ModelConfig
-from aphrodite.common.utils import is_list_of
-from aphrodite.inputs.registry import InputContext
-from aphrodite.transformers_utils.processor import get_image_processor
-
-from .base import MultiModalData, MultiModalInputs, MultiModalPlugin
-
-cached_get_image_processor = lru_cache(get_image_processor)
+from .base import MediaIO
 
 
-class ImagePlugin(MultiModalPlugin):
-    """Plugin for image data."""
+def rescale_image_size(image: Image.Image,
+                       size_factor: float,
+                       transpose: int = -1) -> Image.Image:
+    """Rescale the dimensions of an image by a constant factor."""
+    new_width = int(image.width * size_factor)
+    new_height = int(image.height * size_factor)
+    image = image.resize((new_width, new_height))
+    if transpose >= 0:
+        image = image.transpose(Image.Transpose(transpose))
+    return image
 
-    def get_data_key(self) -> str:
-        return "image"
 
-    def _get_hf_image_processor(
+class ImageMediaIO(MediaIO[Image.Image]):
+
+    def __init__(self, *, image_mode: str = "RGB") -> None:
+        super().__init__()
+
+        self.image_mode = image_mode
+
+    def load_bytes(self, data: bytes) -> Image.Image:
+        image = Image.open(BytesIO(data))
+        image.load()
+        return image.convert(self.image_mode)
+
+    def load_base64(self, media_type: str, data: str) -> Image.Image:
+        return self.load_bytes(base64.b64decode(data))
+
+    def load_file(self, filepath: Path) -> Image.Image:
+        image = Image.open(filepath)
+        image.load()
+        return image.convert(self.image_mode)
+
+    def encode_base64(
         self,
-        model_config: ModelConfig,
-        mm_processor_kwargs: Optional[Dict[str, Any]] = None,
-    ):
-        if mm_processor_kwargs is None:
-            mm_processor_kwargs = {}
-        return cached_get_image_processor(
-            model_config.model,
-            trust_remote_code=model_config.trust_remote_code,
-            **mm_processor_kwargs)
+        media: Image.Image,
+        *,
+        image_format: str = "JPEG",
+    ) -> str:
+        image = media
 
-    def _default_input_mapper(
-        self,
-        ctx: InputContext,
-        data: MultiModalData[object],
-        **mm_processor_kwargs,
-    ) -> MultiModalInputs:
-        model_config = ctx.model_config
+        with BytesIO() as buffer:
+            image = image.convert(self.image_mode)
+            image.save(buffer, image_format)
+            data = buffer.getvalue()
 
-        # Processed by input processor
-        if isinstance(data, BatchFeature):
-            return MultiModalInputs(data.data)
+        return base64.b64encode(data).decode('utf-8')
 
-        # PIL image
-        if isinstance(data, Image.Image) or is_list_of(data, Image.Image):
-            image_processor = self._get_hf_image_processor(
-                model_config,
-                mm_processor_kwargs,
-            )
-            if image_processor is None:
-                raise RuntimeError("No HuggingFace processor is available "
-                                   "to process the image object")
-            try:
-                # NOTE: It may make sense to forward the mm_processor_kwargs
-                # here too. For now, to keep it simple, we only allow it be
-                # used for the initialization call though, just in case the
-                # signatures of the preprocessor initializer don't match
-                # preprocess()
-                batch_data = image_processor \
-                    .preprocess(data, return_tensors="pt") \
-                    .data
-            except Exception:
-                logger.error(f"Failed to process image ({data})")
-                raise
 
-            return MultiModalInputs(batch_data)
+class ImageEmbeddingMediaIO(MediaIO[torch.Tensor]):
 
-        # Image embedding
-        elif isinstance(data, torch.Tensor) or is_list_of(data, torch.Tensor):
-            return MultiModalInputs({"image_embeds": data})
+    def __init__(self) -> None:
+        super().__init__()
 
-        raise TypeError(f"Invalid image type: {type(data)}")
+    def load_bytes(self, data: bytes) -> torch.Tensor:
+        buffer = BytesIO(data)
+        return torch.load(buffer, weights_only=True)
 
-    def _default_max_multimodal_tokens(self, ctx: InputContext) -> int:
-        return 3000
+    def load_base64(self, media_type: str, data: str) -> torch.Tensor:
+        return self.load_bytes(base64.b64decode(data))
+
+    def load_file(self, filepath: Path) -> torch.Tensor:
+        return torch.load(filepath, weights_only=True)
+
+    def encode_base64(self, media: torch.Tensor) -> str:
+        return base64.b64encode(media.numpy()).decode('utf-8')
